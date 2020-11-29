4d61362aac12f8a0d9adc49d09e67e39cad62dd7
chienlove <boypink93@gmail.com>
2020-11-29 10:50:41 +0700

    -

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function removeCharRegExp(string) {
 }
 
 async function addDepiction(pathF, info) {
-   if (!info.Depiction?.match(new RegExp(removeCharRegExp(`${env.Depiction}${info.Package}@${info.Version}`)))) {
+   if (!info.Depiction || !info.Depiction.match(new RegExp(removeCharRegExp(`${env.Depiction}${info.Package}@${info.Version}`)))) {
 
       const pathTmp = `${__dirname}/tmp`
 
